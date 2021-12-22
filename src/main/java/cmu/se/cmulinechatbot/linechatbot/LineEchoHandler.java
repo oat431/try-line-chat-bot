23a3bd084b4e0f12bea1eb91ef94dec876abc4d1
oat431 <oat431@gmail.com>
2021-12-22 15:09:29 +0700

    [😢 add] : echo handler

---
+++ src/main/java/cmu/se/cmulinechatbot/linechatbot/LineEchoHandler.java
@@ -0,0 +1,25 @@
+package cmu.se.cmulinechatbot.linechatbot;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@LineMessageHandler
+@Slf4j
+public class LineEchoHandler {
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        log.info("event: " + event);
+        final String originalMessageText = event.getMessage().getText();
+        return new TextMessage(originalMessageText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}
